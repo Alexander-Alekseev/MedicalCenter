341e5b2fd3e7b03dd95b8dcc342f59046ac36413
Alexander Alekseev <alexander.alekseev@indeed-id.com>
2019-09-06 14:47:46 +0300

    Initial commit

---
+++ MedicalCenter/MedicalCenter/Pages/MenuPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedicalCenter.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MedicalCenter.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new HomeMenuViewModel();
+        }
+
+        private void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+        }
+    }
+}
---
+++ MedicalCenter/MedicalCenter/ViewModels/HomeMenuViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using MedicalCenter.Models;
+
+namespace MedicalCenter.ViewModels
+{
+    public class HomeMenuViewModel
+    {
+        public IList<HomeMenu> lstHomeMenu { get; set; }
+
+        public HomeMenuViewModel()
+        {
+            lstHomeMenu = new List<HomeMenu>();
+            GenerateCardModel();
+        }
+
+        private void GenerateCardModel()
+        {
+            lstHomeMenu.Add(new HomeMenu()
+            {
+                Title = "Request Appointment",
+                Icon = "icon_appointment.png",
+                BackgroundColor = "#479589"
+            });
+            lstHomeMenu.Add(new HomeMenu()
+            {
+                Title = "Order Prescription",
+                Icon = "icon_prescription.png",
+                BackgroundColor = "#4383D9"
+            });
+        }
+    }
+}
